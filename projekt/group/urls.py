3199af14651dadb45ad87038fa97e98e7bd8ff9e
Notcatman <15.03.dato.dato@gmail.com>
2026-02-02 04:13:31 +0400

    ...

--- projekt/group/urls.py
+++ projekt/group/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', list, name='tasks'),
-    path("task/<int:id>", detail, name='detail_page')
+    path("task/<int:id>", detail, name='detail_page'),
+    path('add/', add_task, name='add_task'),
+    path("delete/<list_id>", delete, name="delete"),
+    path("cross/<list_id>", cross, name="cross"),
+    path("uncross/<list_id>", uncross, name="uncross")
 ]
--- projekt/group/views.py
+++ projekt/group/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
+from .forms import TaskForm
 
 
 def list(request):
     tasks = Task.objects.all()
+    form = TaskForm()
     
     context = {
         'tasks': tasks,
+        'form': form,
     }
 
     return render(request, 'base.html', context)
+
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks')
+        
+        tasks = Task.objects.all()
+        context = {
+            'tasks': tasks,
+            'form': form,
+        }
+        return render(request, 'base.html', context)
+    return redirect('tasks')
+    
+
+    
 
 def detail(request, id):
     task = get_object_or_404(Task, id=id)
@@ -20,4 +42,20 @@ def detail(request, id):
 
     return render(request, 'detail.html', context)
 
+def delete(request, list_id):
+    item = Task.objects.get(pk=list_id)
+    item.delete()
+    return redirect('tasks')
+
     
+def cross(request, list_id):
+    item = Task.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('tasks')
+
+def uncross(request, list_id):
+    item = Task.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('tasks')
